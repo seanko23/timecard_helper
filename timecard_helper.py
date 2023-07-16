059bfbcae8e5b205aa1fe27fa74087ee2c8df923
seanko23 <seanko@Seans-MacBook-Pro.local>
2023-07-16 01:20:13 -0400

    cleaned up functions by utilizing open_file function

--- timecard_helper.py
+++ timecard_helper.py
@@ -12,14 +12,14 @@ class Email:
         data = json.load(f)
         f.close()
         
+        return data
+        
     def get_subject(self): # not calling open_file here, may need to adjust. Looking into it 
         #also reviewing if this qualifies for staticmethod
         subject_list = []
         
-        for i in data['value']:
+        for i in self.open_file()['value']:
             subject_list.append(i['subject'])
-        
-#        f.close()
         
         return subject_list
     
@@ -28,12 +28,9 @@ class Email:
         
 
     def get_case_number(self):
-        #self.subject_list = subject_list
-        self.get_subject()
-        
         case_number_list = []
         
-        for i in subject_list:
+        for i in self.get_subject():
             case_number = re.findall(r'(\d{6})', i) # at this point case_number is a list type
 
             for x in case_number:
@@ -44,20 +41,18 @@ class Email:
         return case_number_list
     
     def get_client_code(self):
-        #self.subject_list = subject_list
-        self.get_subject()
         client_code_list = []
         
-        for i in subject_list:
+        for i in self.get_subject():
             case_number = re.findall(r'(\d{6})', i)
             client_code = re.findall(r'(\S{4})', i)
 
             for x in client_code: # At this point nested list is called
                 client_code = x.upper()
-                if client_code in client_list: # If match is found the for loop breaks here
+                if client_code in Email.client_list: # If match is found the for loop breaks here
                     break
                     
-                elif client_code not in client_list: # Here match is not found
+                elif client_code not in Email.client_list: # Here match is not found
                     no_client_code = "UNKNOWN"
                     client_code = None
     
@@ -73,3 +68,6 @@ class Email:
 
 # The end result should look like this: {Date: [case#:client code, case#:client code]...}
 # Maybe I want to grab the subject as well
+
+print(Email('sample_json_message.json').get_client_code_and_case_number())
+#print(Email('sample_json_message.json').get_subject())

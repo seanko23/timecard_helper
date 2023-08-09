b5ef570334162b37d11ea4d3ca9b4de4f887595c
seanko23 <seanko23@gmail.com>
2023-08-08 23:25:07 -0400

    cleaned up sample data

--- app_constants.py
+++ app_constants.py
@@ -1,10 +1,10 @@
 # Please define your client codes - 4 letters only
-client_list = ['TRSC', 'VABC', 'CROC', 'EXLD']
+client_list = ['ABCD', 'EFGH', 'WXYZ']
 
 # Please give start and end dates where you want timecard statistics for (ex. ['23-07-11','23-07-14'])
 # If user has defined the dates already from Postman, your can pass through an empty list
 # If wanting for just 1 day, put the same date twice in below list
-date_list = ['23-07-11','23-07-12']
+date_list = ['23-07-11','23-07-15']
 
 
 # Add an option to bypass date_list entry
